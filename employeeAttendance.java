f1aac117f92b4f14d3e8b82b2964a512005eb961
himanshunete <himanshuneteh@gmail.com>
2020-12-22 19:12:24 +0530

    UC-1: Checking the Attendance of Employees

---
+++ employeeAttendance.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+class employeeAttendance {
+      public static void main(String[] args)
+      {            
+            Scanner sc = new Scanner(System.in);
+            int attendance = sc.nextInt();
+            switch (attendance) {
+            case 1:
+                  System.out.println("Employee is present");
+                  break;
+            case 2:
+                  System.out.println("Employee is not present");
+                  break;
+            }
+      }
+}
+
+
