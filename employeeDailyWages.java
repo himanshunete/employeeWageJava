2efff097a80c4c18c37c8db18aa46d3f704db2c8
himanshunete <himanshuneteh@gmail.com>
2020-12-22 19:40:19 +0530

    UC-2: Checking the Daily Wages of Employees

---
+++ employeeDailyWages.java
@@ -0,0 +1,11 @@
+
+class employeeDailyWages {
+      public static void main(String[] args)
+      {
+            int WagePerHour=20;
+            int FullDayHour=8;
+            int dailyEmployeeWage=WagePerHour*FullDayHour;
+            System.out.println( dailyEmployeeWage);
+       }
+}
+
