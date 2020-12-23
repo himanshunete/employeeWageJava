7289a443a2890c532bdb447f84dbe3de7c05843e
himanshunete <himanshuneteh@gmail.com>
2020-12-23 12:07:42 +0530

    UC-5: Checking the Wage per month

---
+++ wagePerMonth.java
@@ -0,0 +1,11 @@
+
+
+class wagePerMonth{
+  public static void main(String[] args){
+     int WorkingDayPerMonth=20;
+     int WagePerDay=160;
+     int WagePerMonth=WagePerDay*WorkingDayPerMonth;
+     System.out.println(WorkingDayPerMonth + " Days per month ");
+     System.out.println(WagePerMonth + " Rs ");
+  }
+}
