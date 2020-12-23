3e0959a2f61e67442c618497d25883db13cc3710
himanshunete <himanshuneteh@gmail.com>
2020-12-23 15:34:16 +0530

    UC-3: Checking the Part Time n Wage of Employees

--- employeePartTime.java
+++ employeePartTime.java
@@ -1,11 +1,16 @@
 
-public class employeePartTime {
-      public static void main(String[] args)
-      {
+class empPartTime {
             int PartTimeHour=8 ;
             int PartTimeWagePerHour=20 ;
             int PartTimeWage=PartTimeHour*PartTimeWagePerHour ;
-            System.out.println(PartTimeWage + " Rs " + " for total " + PartTimeHour + " hours");
-       }
+            public void Check(){
+                  System.out.println(PartTimeWage + " Rs " + " for total " + PartTimeHour + " hours");
+            }
 }
 
+public class employeePartTime {
+      public static void main(String[] args){
+            empPartTime check = new empPartTime();
+            check.Check();
+      }
+}
