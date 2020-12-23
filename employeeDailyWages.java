9a565491b5980464eda4f902c8f51288cb5013da
himanshunete <himanshuneteh@gmail.com>
2020-12-23 14:51:57 +0530

    UC-2: Checking the Daily Wages of Employees

--- employeeDailyWages.java
+++ employeeDailyWages.java
@@ -1,22 +1,3 @@
-import java.util.Scanner;
-class employeeAttendance {
-      public static void main(String[] args)
-      {
-            Scanner sc = new Scanner(System.in);
-            int attendance = sc.nextInt();
-            switch (attendance) {
-            case 1:
-                  System.out.println("Employee is present");
-                  break;
-            case 2:
-                  System.out.println("Employee is not present");
-                  break;
-            }
-      }
-}
-
-
-
 public class employeeDailyWages {
       public static void main(String[] args)
       {
