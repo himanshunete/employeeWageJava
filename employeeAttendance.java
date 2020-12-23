b15b18951bb710873ed0d539dce51716416048d2
himanshunete <himanshuneteh@gmail.com>
2020-12-23 15:20:25 +0530

    UC-1: Checking the Attendance of employees

--- employeeAttendance.java
+++ employeeAttendance.java
@@ -1,17 +1,24 @@
-
 import java.util.Random;
-class employeeAttendance {
-      public static void main(String[] args)
-      {
+class empAttendance {
             Random num = new Random();
-            int attendance = num.nextInt(2);
-            if (attendance == 0) {
-                  System.out.println("Employee is present");
+            int attendance; 
+            public void Check(){
+                  attendance = num.nextInt(2);
+                  if (attendance == 0) {
+                        System.out.println("Employee is present");
+                  }
+                  else {
+                        System.out.println("Employee is not present");
+                  }
             }
-            else {
-                  System.out.println("Employee is not present");
-            }
-      }
+      
 }
 
 
+public class employeeAttendance {
+      public static void main(String[] args)
+      {
+            empAttendance check = new empAttendance();
+            check.Check();
+      }
+}
