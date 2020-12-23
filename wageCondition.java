e8fc13f2ce0d2b390ab4dd705c0da67c114c87a9
himanshunete <himanshuneteh@gmail.com>
2020-12-23 15:55:13 +0530

    UC-5: Checking wage condition for 100 hours

--- wageCondition.java
+++ wageCondition.java
@@ -1,22 +1,30 @@
 import java.util.Random;
-class wageCondition{
-   public static void main(String[] args){
+class condition{
       int WagePerHour=20;
       int TotalWagePerMonth=0;
       Random num = new Random();
       int fullTimeOrPartTime;
       int hours;
-      for(hours=1;hours<=100;hours++){
-         fullTimeOrPartTime=num.nextInt(2);
-         if(fullTimeOrPartTime == 0){
-            System.out.println(" Full time hour ");
-            TotalWagePerMonth=TotalWagePerMonth + WagePerHour;
-         }
-         else{
-            System.out.println(" Part time hour ");
-            TotalWagePerMonth=TotalWagePerMonth + WagePerHour;
-         }
+      public void Check(){
+            for(hours=1;hours<=100;hours++){
+               fullTimeOrPartTime=num.nextInt(2);
+               if(fullTimeOrPartTime == 0){
+                  System.out.println(" Full time hour ");
+                  TotalWagePerMonth=TotalWagePerMonth + WagePerHour;
+               }
+               else{
+                  System.out.println(" Part time hour ");
+                  TotalWagePerMonth=TotalWagePerMonth + WagePerHour;
+               }
+            }
+            System.out.println(TotalWagePerMonth + " is total wage per month");
       }
-      System.out.println(TotalWagePerMonth + " is total wage per month");
+}
+
+
+class wageCondition{
+   public static void main(String[] args){
+      condition check = new condition();
+      check.Check();
    }
 }
