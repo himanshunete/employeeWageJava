23747122edb6fb00900fb3edb4b309d84204f063
himanshunete <himanshuneteh@gmail.com>
2020-12-23 14:57:43 +0530

    UC-3: Checking the Part Time n Wage of Employees

--- employeePartTime.java
+++ employeePartTime.java
@@ -1,29 +1,3 @@
-import java.util.Scanner;
-class employeeAttendance {
-      public static void main(String[] args)
-      {
-            Scanner sc = new Scanner(System.in);
-            int attendance = sc.nextInt();
-            switch (attendance) {
-            case 1:
-                  System.out.println("Employee is present");
-                  break;
-            case 2:
-                  System.out.println("Employee is not present");
-                  break;
-            }
-      }
-}
-
-class employeeDailyWages {
-      public static void main(String[] args)
-      {
-            int WagePerHour=20;
-            int FullDayHour=8;
-            int dailyEmployeeWage=WagePerHour*FullDayHour;
-            System.out.println( dailyEmployeeWage);
-       }
-}
 
 public class employeePartTime {
       public static void main(String[] args)
